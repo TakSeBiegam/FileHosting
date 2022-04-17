01011ebadea98e77cc6a6957089eeed9b10e84ba
TakSeBiegam <63842313+TakSeBiegam@users.noreply.github.com>
2022-04-17 09:19:14 +0200

    Upgraded Download system
    - Fixed Searching and downloading system

--- Hosting.go
+++ Hosting.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -22,6 +21,10 @@ func cutString(str string) string {
 	return str[7:]
 }
 
+func addPortToCors(w *http.ResponseWriter, port string) { //variable w is obviously | port should be a set with numbers for ex. 8080 or 2137
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:"+port)
+}
+
 /////////////////////////
 //                     //
 /* SERVER SIDE ACTIONS */
@@ -29,18 +32,20 @@ func cutString(str string) string {
 /////////////////////////
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
+	addPortToCors(&w, "*")
+
 	fmt.Fprintf(w, "Uploading File\n")
 
 	r.ParseMultipartForm(10 << 20) //10MB max file size -> http://orokepal.pl/tabele/Matematyka/potegi2.html
 	file, handler, err := r.FormFile("uploadingFile")
 	if err != nil {
 		fmt.Println("Error while uploading\n")
+		fmt.Fprintf(w, "Error while uploading - Contact with administrator\n")
 		fmt.Println(err)
 		return
 	}
 
 	defer file.Close()
-	fmt.Printf("AFTER STEP ONE\n")
 	var pathDirection string = "Go/temp-images" //path where files should be stored
 
 	//tip for tempFile.Name()
@@ -48,14 +53,15 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	specialID := strings.Trim(tempFile.Name(), handler.Filename)                 //Removing filename from specialID
 	specialID = strings.Trim(specialID, "Go/temp-images\\")                      //Removing path from specialID
 	specialID = cutString(specialID)
-	//Here some magic because we are removing upload- from specialID which is using very dangerous function CutString to remove "upload-" from specialID
+	//Here some magic because we are removing upload- from specialID by using very dangerous function CutString()
 
 	//WARNING//
 	//IF YOU EDITING SOMETHING ABOVE YOU NEED TO EDIT cutString() FUNCTION TOO !!!
 
 	// <<< TURN BUG INTO FUTURE BELOW >>>
-	fmt.Fprintf(w, "Saved on server with name: %+v \n", handler.Filename)
+	fmt.Fprintf(w, "Your File Name: %+v \n", handler.Filename)
 	fmt.Fprintf(w, "Saved on server with special ID: %+v \n", specialID)
+	fmt.Fprintf(w, "Save your file name and special ID if you wanna download your file later: !")
 
 	if err != nil {
 		fmt.Println(err)
@@ -79,14 +85,16 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 func searchFile(w http.ResponseWriter, r *http.Request) {
 	//Path where you want to search files
 	pathDirection := "C:\\IT\\programming\\Go\\temp-images\\"
-	Openfile, err := os.Open(pathDirection + "upload-" + r.FormValue("fileName"))
+	searchingFile, err := os.Open(pathDirection + "upload-" + r.FormValue("fileName"))
 	if err != nil {
 		fmt.Println(err)                 //COMENT FOR SERVER
 		fmt.Fprintf(w, "File not found") //COMMENT FOR USER ON BROWSER
 		return
 	}
-	fmt.Printf("%+v\n", Openfile.Name())
-	io.Copy(w, Openfile)
+	w.Header().Set("Content-Disposition", "attachment; filename="+r.FormValue("fileName"))
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+
+	defer searchingFile.Close()
 }
 
 ///////////////////////
